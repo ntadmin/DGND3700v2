51dce1b6843a5b218a573cb97cc5d4b528b51a6e
Neil Townsend <neil@the-townsends.net>
2016-06-27 18:45:34 +0100

    adblock http server

---
+++ extra_binaries/src/adblock_httpd/httpd.c
@@ -0,0 +1,342 @@
+/* Based on J. David's webserver */
+/* This is a simple webserver.
+ * Created November 1999 by J. David Blackstone.
+ * Brutalised to a one file returned by Neil Townsend
+ */
+/* This program compiles for Sparc Solaris 2.6.
+ * To compile for Linux:
+ *  1) Comment out the #include <pthread.h> line.
+ *  2) Comment out the line that defines the variable newthread.
+ *  3) Comment out the two lines that run pthread_create().
+ *  4) Uncomment the line that runs accept_request().
+ *  5) Remove -lsocket from the Makefile.
+ */
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <strings.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+#define ADBLOCK_PORT 8105
+#define PATH_TO_ONLY_FILE "/www.eng/adblock-text.htm"
+
+#define ISspace(x) isspace((int)(x))
+
+#define SERVER_STRING "Server: an_httpd/0.0.1\r\n"
+
+void accept_request(int);
+void bad_request(int);
+void cat(int, FILE *);
+void cannot_execute(int);
+void error_die(const char *);
+void execute_cgi(int, const char *, const char *, const char *);
+int get_line(int, char *, int);
+void headers(int, const char *);
+void not_found(int);
+void serve_file(int, const char *);
+int startup(u_short *);
+void unimplemented(int);
+
+/**********************************************************************/
+/* A request has caused a call to accept() on the server port to
+ * return.  Process the request appropriately.
+ * Parameters: the socket connected to the client */
+/**********************************************************************/
+void accept_request(int client)
+{
+ char buf[1024];
+ int numchars;
+ char method[255];
+ char url[255];
+ size_t i, j;
+ int cgi = 0;      /* becomes true if server decides this is a CGI
+                    * program */
+ char *query_string = NULL;
+
+ numchars = get_line(client, buf, sizeof(buf));
+ i = 0; j = 0;
+ while (!ISspace(buf[j]) && (i < sizeof(method) - 1))
+ {
+  method[i] = buf[j];
+  i++; j++;
+ }
+ method[i] = '\0';
+
+ if (strcasecmp(method, "GET") && strcasecmp(method, "POST"))
+ {
+  unimplemented(client);
+  return;
+ }
+
+ if (strcasecmp(method, "POST") == 0)
+  cgi = 1;
+
+ i = 0;
+ while (ISspace(buf[j]) && (j < sizeof(buf)))
+  j++;
+ while (!ISspace(buf[j]) && (i < sizeof(url) - 1) && (j < sizeof(buf)))
+ {
+  url[i] = buf[j];
+  i++; j++;
+ }
+ url[i] = '\0';
+
+ if (strcasecmp(method, "GET") == 0)
+ {
+  query_string = url;
+  while ((*query_string != '?') && (*query_string != '\0'))
+   query_string++;
+  if (*query_string == '?')
+  {
+   cgi = 1;
+   *query_string = '\0';
+   query_string++;
+  }
+ }
+
+ serve_file(client, PATH_TO_ONLY_FILE);
+
+ close(client);
+}
+
+/**********************************************************************/
+/* Inform the client that a request it has made has a problem.
+ * Parameters: client socket */
+/**********************************************************************/
+void bad_request(int client)
+{
+    serve_file(client, PATH_TO_ONLY_FILE);
+}
+
+/**********************************************************************/
+/* Put the entire contents of a file out on a socket.  This function
+ * is named after the UNIX "cat" command, because it might have been
+ * easier just to do something like pipe, fork, and exec("cat").
+ * Parameters: the client socket descriptor
+ *             FILE pointer for the file to cat */
+/**********************************************************************/
+void cat(int client, FILE *resource)
+{
+ char buf[1024];
+
+ fgets(buf, sizeof(buf), resource);
+ while (!feof(resource))
+ {
+  send(client, buf, strlen(buf), 0);
+  fgets(buf, sizeof(buf), resource);
+ }
+}
+
+/**********************************************************************/
+/* Inform the client that a CGI script could not be executed.
+ * Parameter: the client socket descriptor. */
+/**********************************************************************/
+void cannot_execute(int client)
+{
+    serve_file(client, PATH_TO_ONLY_FILE);
+}
+
+/**********************************************************************/
+/* Print out an error message with perror() (for system errors; based
+ * on value of errno, which indicates system call errors) and exit the
+ * program indicating an error. */
+/**********************************************************************/
+void error_die(const char *sc)
+{
+ perror(sc);
+ exit(1);
+}
+
+/**********************************************************************/
+/* Execute a CGI script.  Will need to set environment variables as
+ * appropriate.
+ * Parameters: client socket descriptor
+ *             path to the CGI script */
+/**********************************************************************/
+void execute_cgi(int client, const char *path,
+                 const char *method, const char *query_string)
+{
+    serve_file(client, PATH_TO_ONLY_FILE);
+}
+
+/**********************************************************************/
+/* Get a line from a socket, whether the line ends in a newline,
+ * carriage return, or a CRLF combination.  Terminates the string read
+ * with a null character.  If no newline indicator is found before the
+ * end of the buffer, the string is terminated with a null.  If any of
+ * the above three line terminators is read, the last character of the
+ * string will be a linefeed and the string will be terminated with a
+ * null character.
+ * Parameters: the socket descriptor
+ *             the buffer to save the data in
+ *             the size of the buffer
+ * Returns: the number of bytes stored (excluding null) */
+/**********************************************************************/
+int get_line(int sock, char *buf, int size)
+{
+ int i = 0;
+ char c = '\0';
+ int n;
+
+ while ((i < size - 1) && (c != '\n'))
+ {
+  n = recv(sock, &c, 1, 0);
+  /* DEBUG printf("%02X\n", c); */
+  if (n > 0)
+  {
+   if (c == '\r')
+   {
+    n = recv(sock, &c, 1, MSG_PEEK);
+    /* DEBUG printf("%02X\n", c); */
+    if ((n > 0) && (c == '\n'))
+     recv(sock, &c, 1, 0);
+    else
+     c = '\n';
+   }
+   buf[i] = c;
+   i++;
+  }
+  else
+   c = '\n';
+ }
+ buf[i] = '\0';
+
+ return(i);
+}
+
+/**********************************************************************/
+/* Return the informational HTTP headers about a file. */
+/* Parameters: the socket to print the headers on
+ *             the name of the file */
+/**********************************************************************/
+void headers(int client, const char *filename)
+{
+ char buf[1024];
+ (void)filename;  /* could use filename to determine file type */
+
+ strcpy(buf, "HTTP/1.0 200 OK\r\n");
+ send(client, buf, strlen(buf), 0);
+ strcpy(buf, SERVER_STRING);
+ send(client, buf, strlen(buf), 0);
+ sprintf(buf, "Content-Type: text/html\r\n");
+ send(client, buf, strlen(buf), 0);
+ strcpy(buf, "\r\n");
+ send(client, buf, strlen(buf), 0);
+}
+
+/**********************************************************************/
+/* Give a client a 404 not found status message. */
+/**********************************************************************/
+void not_found(int client)
+{
+    serve_file(client, PATH_TO_ONLY_FILE);
+}
+
+/**********************************************************************/
+/* Send a regular file to the client.  Use headers, and report
+ * errors to client if they occur.
+ * Parameters: a pointer to a file structure produced from the socket
+ *              file descriptor
+ *             the name of the file to serve */
+/**********************************************************************/
+void serve_file(int client, const char *filename)
+{
+ FILE *resource = NULL;
+ int numchars = 1;
+ char buf[1024];
+
+ buf[0] = 'A'; buf[1] = '\0';
+ while ((numchars > 0) && strcmp("\n", buf))  /* read & discard headers */
+  numchars = get_line(client, buf, sizeof(buf));
+
+ resource = fopen(filename, "r");
+ if (resource == NULL)
+  not_found(client);
+ else
+ {
+  headers(client, filename);
+  cat(client, resource);
+ }
+ fclose(resource);
+}
+
+/**********************************************************************/
+/* This function starts the process of listening for web connections
+ * on a specified port.  If the port is 0, then dynamically allocate a
+ * port and modify the original port variable to reflect the actual
+ * port.
+ * Parameters: pointer to variable containing the port to connect on
+ * Returns: the socket */
+/**********************************************************************/
+int startup(u_short *port)
+{
+ int httpd = 0;
+ struct sockaddr_in name;
+
+ httpd = socket(PF_INET, SOCK_STREAM, 0);
+ if (httpd == -1)
+  error_die("socket");
+ memset(&name, 0, sizeof(name));
+ name.sin_family = AF_INET;
+ name.sin_port = htons(*port);
+ name.sin_addr.s_addr = htonl(INADDR_ANY);
+ if (bind(httpd, (struct sockaddr *)&name, sizeof(name)) < 0)
+  error_die("bind");
+ if (*port == 0)  /* if dynamically allocating a port */
+ {
+  socklen_t namelen = sizeof(name);
+  if (getsockname(httpd, (struct sockaddr *)&name, &namelen) == -1)
+   error_die("getsockname");
+  *port = ntohs(name.sin_port);
+ }
+ if (listen(httpd, 5) < 0)
+  error_die("listen");
+ return(httpd);
+}
+
+/**********************************************************************/
+/* Inform the client that the requested web method has not been
+ * implemented.
+ * Parameter: the client socket */
+/**********************************************************************/
+void unimplemented(int client)
+{
+    serve_file(client, PATH_TO_ONLY_FILE);
+}
+
+/**********************************************************************/
+
+int main(void)
+{
+ int server_sock = -1;
+ u_short port = ADBLOCK_PORT;
+ int client_sock = -1;
+ struct sockaddr_in client_name;
+ socklen_t client_name_len = sizeof(client_name);
+
+ server_sock = startup(&port);
+ printf("httpd running on port %d\n", port);
+
+ while (1)
+ {
+  client_sock = accept(server_sock,
+                       (struct sockaddr *)&client_name,
+                       &client_name_len);
+  if (client_sock == -1)
+   error_die("accept");
+  accept_request(client_sock);
+// if (pthread_create(&newthread , NULL, accept_request, client_sock) != 0)
+//   perror("pthread_create");
+ }
+
+ close(server_sock);
+
+ return(0);
+}
